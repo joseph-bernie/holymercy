e7de6f7a5ab7336277764c8f37ed5dc5b70dcff8
joseph-bernie <75547224+joseph-bernie@users.noreply.github.com>
2020-12-20 17:50:16 -0700

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,128 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1"/>
+<link href="CSS/contact.css" rel="stylesheet" type="text/css"/>
+<link href="CSS/styles.css" rel="stylesheet" type="text/css"/>
+
+<link href="CSS/survey.css" rel="stylesheet" type="text/css"/>
+	<!--Google Fonts-->
+	<link href="https://fonts.googleapis.com/css?family=Satisfy" rel="stylesheet"/>
+ <!--sweetalert2-->
+	<script src="JS/formsubmit.js" defer></script>
+	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@8" defer></script>
+    <link href="CSS/sweetalert2.scss" rel="stylesheet" type="text/scss"/>
+<!--Search Engine Optimization -->    
+<title>Contact Us - holymercy.com </title>
+<meta name="description" content="Please email us at support@holymercy.com concerning spiritual and technical support.
+Our help desk and outreach volunteers are always available to assist with any concerns."/>
+<!--Bing WebMaster <meta> tag containing the authentication code to the <head>-->
+<meta name="msvalidate.01" content="063E773A156618893FBC5B2572C16F74"/> 
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-119988388-1" defer></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-119988388-1');
+</script>
+    
+ <!--favicon-->
+<link rel="shortcut icon" href="img/favicon.webp" type="image/x-icon"/>
+	
+	</head>
+        
+   <body>
+	 
+ 	<header>
+	      <ul>
+			<div class="logo">
+				<img height="48px" src="img/HM logo.webp" alt="Logo"> 
+		</div>
+
+			<ul>&emsp;&emsp;&emsp; &emsp;&emsp;&emsp; &emsp;&emsp;&emsp; &emsp;&emsp;&emsp; &emsp;&emsp;&emsp; 
+				&emsp;&emsp;&emsp; &emsp;&emsp;&emsp; &emsp;&emsp;&emsp; &emsp;&emsp;&emsp; &emsp;&emsp;&emsp; 
+			 <li><a href="index.html" aria-label="Website Homepage"><font size="5">Home</font></a></li>
+				<li><a href="about.html" aria-label="About Us"><font size="5">About</font></a></li>
+				
+			<li><a href="#" aria-label="Ebook Link"><font size="5">Ebooks</font></a> 
+					<ul>
+					<li><a href="ebooks.html" aria-label="About Ebooks">About the Books</a></li>
+					<li><a href="Anthology.html" aria-label="Anthology Book">Anthology</a></li>
+					<li><a href="titles.html" aria-label="Essays">Essays</a></li> 
+					<li><a href="The Circle.html" aria-label="The Circle of Love Book">The Circle of Love</a></li></ul>
+				</li>
+					
+			<li><a href="#" aria-label="Share Link"><font size="5">Share</font></a>	
+				    <ul>
+					<li><a href="share.html" aria-label="Share the Faith">Share the Faith</a></li>  
+					<li><a href="Scripture.html" aria-label="Favorite Scripture">Favorite Scripture</a></li>
+					<li><a href="prayer.php" aria-label="Satisfaction Survey"> Prayer Request</a></li> 
+					<li><a href="contact.php" aria-label="Contact Us">Contact Us</a></li></ul>  
+						 
+					</li></ul></ul>
+         </header>
+		      
+		<div id="google_translate_element"></div><script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, gaTrack: true, gaId: 'UA-119988388-1'}, 'google_translate_element');
+}
+</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit" defer></script>
+      
+
+       		  <div id="script">
+  				<h1> Contact Us </h1>
+     			<br />
+ 					</div>
+                    
+	<img class="img-circle" src="img/Dove.webp" style="width:350px" alt="Dove">
+
+			<br />
+      
+                <div id="script">
+             <p>Email us at <a href="mailto:support@holymercy.com" style="color:rgba(38,32,242,1.00)" aria-label="Contact email">support@holymercy.com</a> or use the contact form below concerning spiritual and technical support.<br>
+        
+         Our help desk and outreach volunteers are always available to assist with any concerns.
+                        &nbsp; It takes 24 hours for us to respond. <br>        
+  
+       &emsp;&emsp;&emsp; &emsp;&emsp;&emsp;&emsp;Our website uses secure encryption for all information sent between your computer and ours.</p>    
+              <br /><br />   
+           
+     <div class="wrapper">
+		<form class="contact-form" action="contactform.php" method="post">
+				  <div class="input-fields">
+					  <input name="name" id="name" type="text" class="input" placeholder="Name">
+					  <input name="mail" id="mail" type="text" class="input" placeholder="Email Address">
+					  <input name="subject" id="subject" type="text" class="input" placeholder="Subject">
+					 
+				  </div>
+			<div class="msg">
+					<textarea name="message" placeholder="Message"></textarea>
+			
+		</div>
+				<input type="submit" name="submit" value="submit" onClick="sweet();">
+			
+	   </form>
+	</div>   
+		<br /><br /><br /><br /> 
+ 
+    
+        
+ <footer>
+		<div id="footmargin">
+			Contact us for any spiritual concerns </div>
+		<div id="footmargin"> © Copyright 2021 - All Rights Reserved </div>
+		<div id="script">
+			<h5>Links</h5> </div>
+		<ul>
+			<li><a href="terms.html" aria-label="Terms and Conditions">Terms and Conditions</a>
+			</li>&emsp;&emsp;&emsp; &emsp;&emsp;
+			<li><a href="contact.php" aria-label="Contact email">support@holymercy.com</a>
+			</li>
+		</ul>
+    </footer>
+  </body>
+</html>
